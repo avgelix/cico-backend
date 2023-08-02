913d3aa0bbf928596fe7a5d515cf91888ecf27d3
Angela Petrone <apetrone@hawk.iit.edu>
2023-08-02 11:54:48 -0500

    Format document for legibility purposes

--- index.js
+++ index.js
@@ -14,36 +14,36 @@ var cors = require('cors');
 
 
 app.use((req, res, next) => {
-    res.cookie('myCrossOriginCookie', 'crossOriginValue', {
-      sameSite: 'None',
-      secure: true,
-      domain: 'localhost:8080', // Replace with your frontend domain
-    });
-    next();
+  res.cookie('myCrossOriginCookie', 'crossOriginValue', {
+    sameSite: 'None',
+    secure: true,
+    domain: 'localhost:8080', // Replace with your frontend domain
   });
+  next();
+});
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with the actual origin of your frontend application
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
-    // Handle the preflight OPTIONS request
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200); // Respond with HTTP 200 OK for OPTIONS requests
-    } else {
-      next();
-    }
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with the actual origin of your frontend application
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Handle the preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // Respond with HTTP 200 OK for OPTIONS requests
+  } else {
+    next();
+  }
+});
 
 app.use(bodyParser.json());
 
 /*app.use(cors());*/
 app.use(session({
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: true,
-    store: memoryStore,
-    cookie: { secure: 'auto' } // "auto" will set Secure based on the request scheme (http/https)
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore,
+  cookie: { secure: 'auto' } // "auto" will set Secure based on the request scheme (http/https)
 }));
 
 app.use(keycloak.middleware());
@@ -55,7 +55,7 @@ var controller = require('./controller/controller.js');
 app.use('/', controller);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 });
 
 
